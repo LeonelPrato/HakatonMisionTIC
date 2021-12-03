02836ff4f91db6864c6cefc0ed05b8aac04fba92
unknown <leonel.prato.mt@correo.usa.edu.co>
2021-12-03 10:33:41 -0500

    Inicializer

---
+++ demo/src/main/java/com/hakaton/demo/BicicletasCompartidasApplication.java
@@ -0,0 +1,13 @@
+package com.hakaton.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BicicletasCompartidasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BicicletasCompartidasApplication.class, args);
+	}
+
+}
